a9b6a3b3558bb8dd202e376a6a7820480338e34f
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-05-07 14:46:04 +0600

    NEW UPDATE FOR LOACAL TO PRO

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function run() {
 
             const updateProduct = {
                 $set: {
-                   ...updatedQuantity
+                    ...updatedProduct
                 },
             };
 
